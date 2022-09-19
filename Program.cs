16e45cadbfc6839179b4929433bd7cd251727617
dmity <dima.antipov92@mail.ru>
2022-09-19 20:39:57 +0300

    Enter array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿//итоговая проверочная работа.
 Console.Clear();
+
+Console.WriteLine("Введите данные через пробел и нажмите Enter");
+string[] arr = Console.ReadLine().Split(' ').ToArray();
+string[] arr2 = new string[arr.Length];
